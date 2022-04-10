29d2fb87bdec56c92b01eab962589f1f4a6770c3
Michell Aagaard Dranig <tiamisse@gmail.com>
2022-04-11 00:05:28 +0200

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // Redux
 import { combineReducers, createStore, applyMiddleware } from 'redux';
-import { employeeReducer } from './services/reducers/employeeReducer';
 import ReduxThunk from 'redux-thunk';
+import { employeeReducer } from './services/reducers/employeeReducer';
 
 // Bootstrap
 import 'bootstrap/dist/css/bootstrap.css';
