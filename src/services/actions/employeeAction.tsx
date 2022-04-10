eaaa3ee7c4ccd3b54beffdf129f798096131a441
Michell Aagaard Dranig <tiamisse@gmail.com>
2022-04-10 22:40:55 +0200

    increase number of employees to fetch

--- src/services/actions/employeeAction.tsx
+++ src/services/actions/employeeAction.tsx
@@ -1,6 +1,6 @@
 export const baseUrl = 'https://randomuser.me/api/';
 
-export const contactInformation = `${baseUrl}?results=15&inc=name,email,phone,cell,nat,location,picture,id,dob`;
+export const contactInformation = `${baseUrl}?results=25&inc=name,email,phone,cell,nat,location,picture,id,dob`;
 
 export const fetchEmployees = () => {
   return async (dispatch: any) => {
